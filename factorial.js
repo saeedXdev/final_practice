5d082ff7cd450b7b71ed7749185353e18bf55792
Ansari Saeed <heyysaeed.dev@gmail.com>
2024-01-05 11:51:19 +0530

    file has been changed

--- factorial.js
+++ factorial.js
@@ -3,11 +3,7 @@ const factorial = function(val)
     if(val===0 || val===1)
         return 1;
 
-<<<<<<< HEAD
-       
-=======
-    return val-1 * factorial(val-1);    
->>>>>>> master
+       return val * factorial(val-1);
 }
 
 
