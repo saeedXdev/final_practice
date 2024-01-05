b14061fc60f605ae152dd759fe36ae14acdf9c20
Ansari Saeed <heyysaeed.dev@gmail.com>
2024-01-05 11:37:20 +0530

    factorial.js first commit

---
+++ factorial.js
@@ -0,0 +1,11 @@
+const factorial = function(val)
+{
+    if(val===0 || val===1)
+        return 1;
+
+    return val * factorial(val-1);    
+}
+
+
+const val = prompt("Please Enter the values : ");
+console.log(factorial(Number(val)))
