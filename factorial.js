d95732abe4cee8a0e94058716fa7135b9d6733f1
shivlal <saeedanwaransari573@gmail.com>
2024-01-05 18:29:31 +0530

    add function added

--- factorial.js
+++ factorial.js
@@ -3,9 +3,12 @@ const factorial = function(val)
     if(val===0 || val===1)
         return 1;
 
-       return val * factorial(val-1);
+       return val * factorial(val-1); 
 }
-
 
 const val = Number(prompt("Please Enter the values : "));
 console.log(factorial(val));
+
+const sum = function(val1 ,val2){
+    return val1 + val2;
+}
